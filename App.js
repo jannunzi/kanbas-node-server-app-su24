2f86c285234f933e920c5a728de6ad04a1d6f4f8
Jose Annunziato <jannunzi@gmail.com>
2024-06-09 13:33:34 -0400

    db

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-
+import UserRoutes from "./Users/routes.js";
 import cors from "cors";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 
 Hello(app);
 Lab5(app);
